b35e20263c7931b5264d11c8bf62358fb26128bb
Joe Colley <josephthco@gmail.com>
2023-09-20 19:50:21 -0400

    MVP (TMP, Damage Peg, Prefabs)

--- DownThrowRemastered/Assets/Scripts/DamagePeg.cs
+++ DownThrowRemastered/Assets/Scripts/DamagePeg.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DamagePeg : Peg
 {
+    [SerializeField] TextMeshProUGUI damageText;
     int damage;
 
     public override void Awake()
     {
         base.Awake();
-        damage = Random.Range(1, 6);
+        SetDamage(Random.Range(1, 6));
+    }
+
+    void SetDamage(int damage)
+    {
+        this.damage = damage;
+        damageText.SetText(damage.ToString());
     }
 
     public override void OnPegHit(Ball ball)
