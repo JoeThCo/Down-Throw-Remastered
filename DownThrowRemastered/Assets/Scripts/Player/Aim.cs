035f2ac533aba173276973d3aa93168befc36344
Joe Colley <josephthco@gmail.com>
2023-09-30 17:07:36 -0400

    Calculated Aim

--- DownThrowRemastered/Assets/Scripts/Player/Aim.cs
+++ DownThrowRemastered/Assets/Scripts/Player/Aim.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class Aim : MonoBehaviour
 {
     [SerializeField] Transform rotatePoint;
+    [SerializeField] AimController aimController;
+    [Space(10)]
     [SerializeField] [Range(5, 10)] float aimSensitivty;
     [SerializeField] float rotationLimit = 75f;
+
+    private const float MIN_PERCENT = -1;
+    private const float MAX_PERCENT = 1;
 
     float lastAngle = 0;
     private Camera cam;
@@ -19,11 +24,13 @@ public class Aim : MonoBehaviour
     float GetShooterAngle(Vector2 mousePos)
     {
         float angle = mousePos.x / (aimSensitivty);
-        return Mathf.Clamp(angle, -1, 1) * rotationLimit;
+        return Mathf.Clamp(angle, MIN_PERCENT, MAX_PERCENT) * rotationLimit;
     }
 
     private void FixedUpdate()
     {
+        if (!aimController.CanAngleChange()) return;
+
         float angle = GetShooterAngle(cam.ScreenToWorldPoint(Input.mousePosition));
         transform.RotateAround(rotatePoint.position, Vector3.forward, -(lastAngle - angle));
 
--- DownThrowRemastered/Assets/Scripts/Player/AimController.cs
+++ DownThrowRemastered/Assets/Scripts/Player/AimController.cs
@@ -9,7 +9,13 @@ public class AimController : MonoBehaviour
     [Space(10)]
     [SerializeField] Transform firePoint;
 
-    [HideInInspector] public bool canShoot = true;
+    private bool canShoot = true;
+
+    private bool isAngleSet = false;
+    private bool isPowerSet = false;
+    private float calculatedPower = 0;
+
+    [SerializeField] AimType aimType;
 
     private const float MIN_POWER_SCALED = .1f;
     private const float MAX_POWER_SCALED = 1f;
@@ -36,16 +42,29 @@ public class AimController : MonoBehaviour
     private void EventManager_OnBallBottoms(Ball ball)
     {
         canShoot = true;
+        ResetCalculated();
     }
 
     private void EventManager_onPlayerShoot()
     {
         Rigidbody2D ball = ItemSpawner.SpawnGame("Ball", firePoint.transform.position).GetComponent<Rigidbody2D>();
-        ball.velocity = -firePoint.transform.up * (GetPlayerPower() * basePower);
+
+        if (aimType == AimType.AllMouse)
+        {
+            Debug.Log(GetPlayerPower());
+            ball.velocity = -firePoint.transform.up * (GetPlayerPower() * basePower);
+        }
+        else if (aimType == AimType.Calculated)
+        {
+            Debug.Log(calculatedPower);
+            ball.velocity = -firePoint.transform.up * (calculatedPower * basePower);
+        }
 
         ItemSpawner.PlaySFX("playerShoot");
         canShoot = false;
     }
+
+
     float GetPlayerPower()
     {
         Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
@@ -53,11 +72,73 @@ public class AimController : MonoBehaviour
         return Mathf.Clamp(power, MIN_POWER_SCALED, MAX_POWER_SCALED);
     }
 
+    public bool CanAngleChange()
+    {
+        return aimType == AimType.AllMouse || aimType == AimType.Calculated && !isAngleSet;
+    }
+
+    public bool CanPowerChange()
+    {
+        return aimType == AimType.AllMouse || aimType == AimType.Calculated && !isPowerSet;
+    }
+
+    void ResetCalculated()
+    {
+        isPowerSet = false;
+        isAngleSet = false;
+        calculatedPower = 0;
+    }
+
+    void Calculated()
+    {
+        if (!isAngleSet)
+        {
+            Debug.Log("Angle Set!");
+            isAngleSet = true;
+        }
+        else
+        {
+            if (!isPowerSet)
+            {
+                calculatedPower = GetPlayerPower();
+                Debug.Log("Power Set @ " + calculatedPower);
+
+                isPowerSet = true;
+            }
+            else
+            {
+                if (isPowerSet && isAngleSet)
+                {
+                    Debug.Log("Fire!");
+                    EventManager.Invoke(CustomEvent.PlayerShootStart);
+                }
+            }
+        }
+    }
+
     private void Update()
     {
-        if (Input.GetMouseButtonUp(0) && canShoot)
+        if (!canShoot) return;
+
+        if (Input.GetMouseButtonUp(0))
         {
-            EventManager.Invoke(CustomEvent.PlayerShootStart);
+            if (aimType == AimType.AllMouse)
+            {
+                EventManager.Invoke(CustomEvent.PlayerShootStart);
+            }
+            else if (aimType == AimType.Calculated)
+            {
+                Calculated();
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (aimType == AimType.Calculated)
+            {
+                Debug.Log("Reset!");
+                ResetCalculated();
+            }
         }
     }
 }
